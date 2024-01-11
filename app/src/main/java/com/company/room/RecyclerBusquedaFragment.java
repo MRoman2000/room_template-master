d4c24a7f256814f2ab8560fd82b87eebfa3a60fa
Roman <madzey71@gmail.com>
2024-01-11 13:14:13 +0100

    Initial commit

---
+++ app/src/main/java/com/company/room/RecyclerBusquedaFragment.java
@@ -0,0 +1,13 @@
+package com.company.room;
+
+import androidx.lifecycle.LiveData;
+
+import java.util.List;
+
+public class RecyclerBusquedaFragment extends RecyclerElementosFragment {
+    @Override
+    LiveData<List<Elemento>> obtenerElementos() {
+        return elementosViewModel.buscar();
+    }
+}
+
